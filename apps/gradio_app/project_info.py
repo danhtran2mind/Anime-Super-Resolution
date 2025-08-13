fffaa5137910d00f815a346dab01ce080aa56e1e
Danh Tran <cprmmb99@gmail.com>
2025-08-13 16:57:40 +0700

    Update project_info.py

--- apps/gradio_app/project_info.py
+++ apps/gradio_app/project_info.py
@@ -1,70 +1,37 @@
-CONTENT_DESCRIPTION = """
-**Anime-Super-Resolution: High-Quality Anime Image Upscaling with Real-ESRGAN**
+NAME = """
+# ControlNet Image Generator 🖌️
 """.strip()
+
+CONTENT_DESCRIPTION = """
+<h3>ControlNet ⚡️ boosts Stable Diffusion with sharp, innovative image generation control 🖌️</h3>
+""".strip()
+
+# CONTENT_IN_1 = """
+# Transforms low-res anime images into sharp, vibrant HD visuals, enhancing textures and details for artwork and games.
+# """.strip()
 
 CONTENT_IN_1 = """
-Transforms low-res anime images into sharp, vibrant HD visuals, enhancing textures and details for artwork and games.
-""".strip()
-
-CONTENT_IN_2 = """
 <p class="source">
-    For more information, you can check out my GitHub repo:<br>
+    For more information, you can check out my GitHub repository and HuggingFace Model Hub:<br>
     Source code: 
-    <a class="badge" href="https://github.com/danhtran2mind/Anime-Super-Resolution">
-        <img src="https://img.shields.io/badge/GitHub-danhtran2mind%2FAnime--Super--Resolution-blue?style=flat&logo=github" alt="GitHub Repo">
+    <a class="badge" href="https://github.com/danhtran2mind/ControlNet-Image-Generator">
+        <img src="https://img.shields.io/badge/GitHub-danhtran2mind%2FControlNet--Image--Generator-blue?style=flat&logo=github" alt="GitHub Repo">
     </a>,
     Model Hub:
-    <a class="badge" href="https://huggingface.co/danhtran2mind/Real-ESRGAN-Anime-finetuning">
-        <img src="https://img.shields.io/badge/HuggingFace-danhtran2mind%2FReal--ESRGAN--Anime--finetuning-yellow?style=flat&logo=huggingface" alt="HuggingFace Model">
+    <a class="badge" href="https://huggingface.co/danhtran2mind/Stable-Diffusion-2.1-Openpose-ControlNet">
+        <img src="https://img.shields.io/badge/HuggingFace-danhtran2mind%2FStable--Diffusion--2.1--Openpose--ControlNet-yellow?style=flat&logo=huggingface" alt="HuggingFace Model">
     </a>.
 </p>
 """.strip()
 
 CONTENT_OUT_1 = """
-This project leverages code from:
 <div class="quote-container">
     <p>
-        The Original Real-ESRGAN 
-        <a class="badge" href="https://github.com/xinntao/Real-ESRGAN">
-            <img src="https://img.shields.io/badge/Built%20on-xinntao%2FReal--ESRGAN-blue?style=flat&logo=github" alt="Built on Real-ESRGAN">
-        </a>
-        by 
-        <a class="badge" href="https://github.com/xinntao">
-            <img src="https://img.shields.io/badge/GitHub-xinntao-blue?style=flat&logo=github" alt="xinntao">
-        </a>, 
-        with our own bug fixes and enhancements available at 
-        <a class="badge" href="https://github.com/danhtran2mind/Real-ESRGAN">
-            <img src="https://img.shields.io/badge/GitHub-danhtran2mind%2FReal--ESRGAN-blue?style=flat&logo=github" alt="Real-ESRGAN Enhancements">
-        </a>.
-        <br>
-        The Inference code
-        <a class="badge" href="https://github.com/ai-forever/Real-ESRGAN">
-            <img src="https://img.shields.io/badge/Built%20on-ai--forever%2FReal--ESRGAN-blue?style=flat&logo=github" alt="Built on Real-ESRGAN">
-        </a>
-        by 
-        <a class="badge" href="https://github.com/ai-forever">
-            <img src="https://img.shields.io/badge/GitHub-ai--forever-blue?style=flat&logo=github" alt="xinntao">
-        </a>, 
-        with our own bug fixes and enhancements available at 
-        <a class="badge" href="https://github.com/danhtran2mind/Real-ESRGAN-inference">
-            <img src="https://img.shields.io/badge/GitHub-danhtran2mind%2FReal--ESRGAN--inference-blue?style=flat&logo=github" alt="Real-ESRGAN Enhancements">
+        This project is built using code from
+        <a class="badge" href="https://github.com/huggingface/diffusers">
+            <img src="https://img.shields.io/badge/Built%20on-huggingface%2Fdiffusers-blue?style=flat&logo=github" alt="Built on Real-ESRGAN">
         </a>.
     </p>
 </div>
 """.strip()
 
-CONTENT_OUT_2 = """
-You can explore more Model Hubs at:
-<div class="quote-container">
-    <p>
-        HuggingFace Model Hub: 
-        <a class="badge" href="https://huggingface.co/ai-forever/Real-ESRGAN">
-            <img src="https://img.shields.io/badge/HuggingFace-ai--forever%2FReal--ESRGAN-yellow?style=flat&logo=huggingface" alt="ai-forever Real-ESRGAN Model">
-        </a>,
-        Real-ESRGAN Model releases: 
-        <a class="badge" href="https://github.com/xinntao/Real-ESRGAN/releases">
-            <img src="https://img.shields.io/badge/GitHub-Real--ESRGAN%2Freleases-blue?style=flat&logo=github" alt="Real-ESRGAN releases">
-        </a>.
-    </p>    
-</div>
-""".strip()
